e92f313020b582ff12acd3308fa87bd85bf3e89e
sleekmason <sleekmason@gmail.com>
2023-08-20 13:58:18 -0500

    Changed welcome script exit button from 'ok' to 'exit'.

--- config/includes.chroot_after_packages/usr/local/bin/welcome.sh
+++ config/includes.chroot_after_packages/usr/local/bin/welcome.sh
@@ -7,14 +7,14 @@ if grep -q "$fs" /proc/partitions; then
   yad --title "Welcome!" --window-icon=/usr/share/icons/ld-icons/paw-color.png \
 --width=488 --height=444 --center --escape-ok --undecorated --skip-taskbar \
 --button=" Begin"!/usr/share/icons/gnome/22x22/places/debian-swirl.png!:"x-terminal-emulator -T 'Customization' -e 'sudo ld-entry -i'" \
---button="gtk-ok:0" \
+--button=" Exit!application-exit:0" \
 --text-info --justify=left --wrap < /usr/share/lilidog/welcome.txt --fontname="JetBrains Mono Light 11" \
 --fore="#C0DDEB"; sed -i '/welcome.sh &/d' ~/.config/openbox/autostart; exit
 else
   yad --notification --image=/usr/share/icons/ld-icons/install.png --text "Install Lilidog ┃ User password ▸ 'user' ┃ Root password ▸ 'live'" --command "gksudo calamares" &
   yad --title "Welcome!" --window-icon=/usr/share/icons/ld-icons/paw-color.png \
 --width=488 --height=520 --center --escape-ok --undecorated --skip-taskbar \
---button="gtk-ok:0" \
+--button=" Exit!application-exit:0" \
 --text-info --justify=left --wrap < /usr/share/lilidog/welcome2.txt --fontname="JetBrains Mono Light 11" \
 --fore="#C0DDEB"; sed -i '/welcome.sh &/d' ~/.config/openbox/autostart; exit
 fi
