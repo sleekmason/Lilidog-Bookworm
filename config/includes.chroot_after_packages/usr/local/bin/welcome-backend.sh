995849d58f4e20c04dd6fcbd742401416f8c8c4b
sleekmason <sleekmason@gmail.com>
2023-08-25 17:57:23 -0500

    adjusted text for dunst and the welcome script.

--- config/includes.chroot_after_packages/usr/local/bin/welcome-backend.sh
+++ config/includes.chroot_after_packages/usr/local/bin/welcome-backend.sh
@@ -9,12 +9,12 @@ if grep -q "$fs" /proc/partitions; then
 --button=" Begin"!/usr/share/icons/gnome/22x22/places/debian-swirl.png!:"x-terminal-emulator -T 'Customization' -e 'sudo ld-entry -i'" \
 --button=" Exit!application-exit:0" \
 --text-info --justify=left --wrap < /usr/share/lilidog/welcome.txt --fontname="JetBrains Mono Light 11" \
---fore="#C0DDEB"; sed -i '/welcome-backend.sh &/d' ~/.config/openbox/autostart; exit
+--fore="#D8E5EB"; sed -i '/welcome-backend.sh &/d' ~/.config/openbox/autostart; exit
 else
   yad --notification --image=/usr/share/icons/ld-icons/install.png --text "Install Lilidog ┃ User password ▸ 'user' ┃ Root password ▸ 'live'" --command "gksudo calamares" &
   yad --title "Welcome!" --window-icon=/usr/share/icons/ld-icons/paw-color.png \
 --width=488 --height=520 --center --escape-ok --undecorated --skip-taskbar \
 --button="gtk-ok:0" \
 --text-info --justify=left --wrap < /usr/share/lilidog/welcome2.txt --fontname="JetBrains Mono Light 11" \
---fore="#C0DDEB"; sed -i '/welcome-backend.sh &/d' ~/.config/openbox/autostart; exit
+--fore="#D8E5EB"; sed -i '/welcome-backend.sh &/d' ~/.config/openbox/autostart; exit
 fi
