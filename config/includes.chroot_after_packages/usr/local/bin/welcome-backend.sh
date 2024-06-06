64748d70ed88901cedf3ab4af6ee446941a783aa
sleekmason <sleekmason@gmail.com>
2024-06-06 00:01:51 -0500

    added installed session one shot

--- config/includes.chroot_after_packages/usr/local/bin/welcome-backend.sh
+++ config/includes.chroot_after_packages/usr/local/bin/welcome-backend.sh
@@ -4,8 +4,9 @@
 full_fs=$(df ~ | tail -1 | awk '{print $1;}')  # find partition
 fs=$(basename "$full_fs")
 if grep -q "$fs" /proc/partitions; then
-xdg-mime default thunar.desktop inode/directory &&
-gsettings set org.gnome.desktop.interface icon-theme "Obsidian-Gray" &&
+#xdg-mime default thunar.desktop inode/directory &&
+#gsettings set org.gnome.desktop.interface icon-theme "Obsidian-Gray" &&
+/usr/local/bin/installed-session &&
   yad --title "Welcome!" --window-icon=/usr/share/icons/ld-icons/paw-color.png \
 --width=488 --height=444 --center --escape-ok --undecorated --skip-taskbar \
 --button=" Begin"!/usr/share/icons/gnome/22x22/places/debian-swirl.png!:"x-terminal-emulator -T 'Customization' -e 'sudo ld-entry -i'" \
