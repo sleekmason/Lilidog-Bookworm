13d955cffaaff2131a796ad63bb7ae5989eb724a
sleekmason <sleekmason@gmail.com>
2024-06-28 10:39:06 -0500

    Terminal color adjustments

--- config/includes.chroot_after_packages/usr/local/bin/welcome-backend.sh
+++ config/includes.chroot_after_packages/usr/local/bin/welcome-backend.sh
@@ -4,8 +4,6 @@
 full_fs=$(df ~ | tail -1 | awk '{print $1;}')  # find partition
 fs=$(basename "$full_fs")
 if grep -q "$fs" /proc/partitions; then
-#xdg-mime default thunar.desktop inode/directory &&
-#gsettings set org.gnome.desktop.interface icon-theme "Obsidian-Gray" &&
 /usr/local/bin/installed-session &&
   yad --title "Welcome!" --window-icon=/usr/share/icons/ld-icons/paw-color.png \
 --width=488 --height=444 --center --escape-ok --undecorated --skip-taskbar \
@@ -14,7 +12,6 @@ if grep -q "$fs" /proc/partitions; then
 --text-info --justify=left --wrap < /usr/share/lilidog/welcome.txt --fontname="JetBrains Mono Light 11" \
 --fore="#E6E6E3"; sed -i '/welcome-backend.sh &/d' ~/.config/openbox/autostart; if ! grep hypervisor /proc/cpuinfo ; then sed -i '/vmrestore.*/d' ~/.config/openbox/autostart; fi; exit
 else
-#gsettings set org.gnome.desktop.interface icon-theme "Obsidian-Gray" &&
 /usr/local/bin/live-session &&
   yad --notification --image=/usr/share/icons/ld-icons/install.png --text "Install Lilidog ┃ User password ▸ 'user' ┃ Root password ▸ 'live'" --command "gksudo calamares" &
   yad --title "Welcome!" --window-icon=/usr/share/icons/ld-icons/paw-color.png \
